2926e70100c583d1163d1ae0e562912d7719bc84
Forrest Voight <forrest@forre.st>
2016-01-21 15:09:12 -0500

    work

--- trajplan.py
+++ trajplan.py
@@ -6,15 +6,15 @@ class CardinalBSpline(object):
     def __init__(self, points, control, degree):
         points = list(points)
         control = map(float, control)
-        assert len(points) == len(control)
+        #assert len(points) == len(control)
         self.points = points
         self.control = control
         self.degree = degree
-        for i in xrange(self.degree):
-            self.points.insert(0, self.points[0])
-            self.control.insert(0, self.control[0])
-            self.points.append(self.points[-1])
-            self.control.append(self.control[-1])
+        #for i in xrange(self.degree):
+        #    self.points.insert(0, self.points[0])
+        #    self.control.insert(0, self.control[0])
+        #    self.points.append(self.points[-1])
+        #    self.control.append(self.control[-1])
     
     def evaluate(self, x):
         assert x >= self.control[0] and x <= self.control[-1]
@@ -28,14 +28,13 @@ class CardinalBSpline(object):
         
         n = self.degree
         d = [self.points[i] for i in xrange(l-n, l+1)]
-        c = [self.control[i] for i in xrange(l-n, l+1)]
-        c = self.control[l-n:]
+        c = [self.control[i] for i in xrange(l-n+1, l-n+1+1+2*n-1)]
         lerp = lambda a, b, x: (1-x)*a + x*b
         for k in xrange(1, n+1):
             d = [lerp(
                 d[i],
                 d[i+1],
-                (x-c[i+k])/(c[i+n+1] - c[i+k]),
+                (x-c[i+k-1])/(c[i+n] - c[i+k-1]),
             ) for i in xrange(n-k+1)]
         assert len(d) == 1
         return d[0]
@@ -44,9 +43,8 @@ class CardinalBSpline(object):
 
 import numpy
 from matplotlib import pyplot
-for deg in [0, 1, 2]: #xrange(10):
-    bs = CardinalBSpline([1, 2, 3, 6, 5, 6, 7], numpy.linspace(0, 1, 7), deg)
-    pyplot.plot(*zip(*[(x, bs.evaluate(x)) for x in numpy.linspace(0, 1, 1000)]))
+bs = CardinalBSpline([0, 0, 0, 6, 0, 0, 0], [-2, -2, -2, -2, -1, 0, 1, 2, 2, 2, 2], 3)
+pyplot.plot(*zip(*[(x, bs.evaluate(x)) for x in numpy.linspace(-2, 2, 1000)]))
 #pyplot.figure(2)
 #for deg in [0, 1, 2]: #xrange(10):
 #    bs = CardinalBSpline([1, 2, 3, 6, 5, 6, 7], numpy.linspace(0, 1, 7), deg)
