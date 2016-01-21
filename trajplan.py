813e0bbd878e2d7a1d4fcaea3b9c1f5f2831a7a0
Forrest Voight <forrest@forre.st>
2016-01-21 09:48:56 -0500

    work

--- trajplan.py
+++ trajplan.py
@@ -1,27 +1,46 @@
 from __future__ import division
 
+import bisect
+
 class CardinalBSpline(object):
-    def __init__(self, points, degree):
+    def __init__(self, points, control, degree):
+        points = list(points)
+        control = map(float, control)
+        assert len(points) == len(control)
         self.points = points
+        self.control = control
         self.degree = degree
+        for i in xrange(self.degree):
+            self.points.insert(0, self.points[0])
+            self.control.insert(0, self.control[0])
+            self.points.append(self.points[-1])
+            self.control.append(self.control[-1])
     
     def evaluate(self, x):
-        l = int(x)
+        assert x >= self.control[0] and x <= self.control[-1]
+        if x <= self.control[0]+1e-6: x = self.control[0]+1e-6
+        if x >= self.control[-1]-1e-6: x = self.control[-1]-1e-6
+        
+        l = bisect.bisect_right(self.control, x) - 1
+        if l == len(self.control)-1: l -= 1
+        assert l >= 0
+        assert self.control[l] <= x < self.control[l+1]
+        
         n = self.degree
-        if l-n < 0: return None
         d = [self.points[i] for i in xrange(l-n, l+1)]
         d_start = l-n
         lerp = lambda a, b, x: (1-x)*a + x*b
         for k in xrange(1, n+1):
-            d = [lerp(d[i-1-d_start], d[i-d_start], (x-i)/(n+1-k)) for i in xrange(l-n+k, l+1)]
+            d = [lerp(d[i-1-d_start], d[i-d_start], (x-self.control[i])/(self.control[i+n+1-k] - self.control[i])) for i in xrange(l-n+k, l+1)]
             d_start = l-n+k
+        assert len(d) == 1
         return d[l-d_start]
 
 
 
 import numpy
 from matplotlib import pyplot
-for deg in xrange(5):
-    bs = CardinalBSpline([1, 2, 3, 6, 5, 6, 7], deg)
-    pyplot.plot(*zip(*[(x, bs.evaluate(x)) for x in numpy.linspace(0, 7, 1000)[:-1]]))
+for deg in [0, 1, 2]: #xrange(10):
+    bs = CardinalBSpline([1, 2, 3, 6, 5, 6, 7], numpy.linspace(0, 1, 7), deg)
+    pyplot.plot(*zip(*[(x, bs.evaluate(x)) for x in numpy.linspace(0, 1, 1000)]))
 pyplot.show()
