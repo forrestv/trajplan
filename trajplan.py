3801f7d7a1cac64257a922fcca9de72011457a4b
Forrest Voight <forrest@forre.st>
2016-01-23 14:38:05 -0500

    cleanup

--- trajplan.py
+++ trajplan.py
@@ -1,6 +1,13 @@
 from __future__ import division
 
 import bisect
+import math
+
+import numpy
+from matplotlib import pyplot, animation
+
+inf = 1e1000; assert math.isinf(inf)
+
 
 class BSpline(object):
     def __init__(self, points, knots, degree, lerp=lambda a, b, x, dxdt: (1-x)*a + x*b):
@@ -51,13 +58,11 @@ class BSpline(object):
         knots = [0] * KNOTDUP + map(float, numpy.linspace(0, 1, 2*DUP + (len(points) - (2*(degree-1))) + degree + 1 - 2 * KNOTDUP)) + [1] * KNOTDUP
         return cls(points, knots, degree, **kwargs)
 
-import numpy
-from matplotlib import pyplot, animation
-a = lambda *x: numpy.array(list(x))
-import math
-
-N = 20
-points = [a(math.cos(i/N*math.pi), math.sin(i/N*math.pi))+numpy.random.randn(2)*.04 for i in xrange(N+1)]
+spline_control_point_count = 20
+spline_control_points = [numpy.array([
+    math.cos(i/spline_control_point_count*math.pi),
+    math.sin(i/spline_control_point_count*math.pi),
+])+numpy.random.randn(2)*.04 for i in xrange(spline_control_point_count+1)]
 
 def mylerp(a, b, x, dxdt):
     if not isinstance(a, dict):
@@ -77,244 +82,179 @@ def mylerp(a, b, x, dxdt):
         a=((1-x)*a['a'] + x*b['a']) + (b['v'] - a['v'])*dxdt + (b['v'] - a['v'])*dxdt,
     )
 
-bs = BSpline.simple_nostop(points, 2, lerp=mylerp)
+bs = BSpline.simple_nostop(spline_control_points, 2, lerp=mylerp)
 
-spline_view_points = [bs.evaluate(x)['p'] for x in numpy.linspace(0, 1, 1000)]
+def w(p, v):
+    return -v
+
+m = 1
+
+u_constraints = [ # halfspace_normal, halfspace_dist; see line_halfspace_intersection
+    (numpy.array([1, 0]), -1),
+    (numpy.array([-1, 0]),-1),
+    (numpy.array([0, 1]), -1),
+    (numpy.array([0, -1]),-1),
+]
+
+range_is_valid = lambda (lo, hi): lo <= hi
+intersect_ranges = lambda (lo1, hi1), (lo2, hi2): (max(lo1, lo2), min(hi1, hi2))
+
+def line_halfspace_intersection(line_start, line_dir, halfspace_normal, halfspace_dist):
+    # halfspace_normal * allowed_point >= halfspace_dist
+    # line is defined by line_start + x * line_dir
+    # returns interval of x that is allowed
+    
+    # halfspace_normal . p >= halfspace_dist
+    # halfspace_normal . (line_start + x * line_dir) >= halfspace_dist
+    # halfspace_normal . line_start + x * halfspace_normal . line_dir >= halfspace_dist
+    # x * (halfspace_normal . line_dir) >= halfspace_dist - halfspace_normal . line_start
+    if halfspace_normal.dot(line_dir) > 0:
+        return ((halfspace_dist - halfspace_normal.dot(line_start)) / halfspace_normal.dot(line_dir), inf)
+    elif halfspace_normal.dot(line_dir) < 0:
+        return (-inf, (halfspace_dist - halfspace_normal.dot(line_start)) / halfspace_normal.dot(line_dir))
+    else: # line is parallel to halfspace boundary
+        if halfspace_normal.dot(line_start) >= halfspace_dist: # everything allowed
+            return (-inf, inf)
+        else: # nothing allowed
+            return (inf, -inf)
+
+def get_allowable_d2s_over_dt2_range(s, ds_over_dt):
+    res = bs.evaluate(s)
+    
+    # u = line_start + d2s/dt2 line_dir
+    line_start = m * res['a'] * ds_over_dt**2 - w(res['p'], res['v'] * ds_over_dt)
+    line_dir = m * res['v']
+    
+    allowed = (-inf, inf)
+    for halfspace_normal, halfspace_dist in u_constraints:
+        this_allowed = line_halfspace_intersection(line_start, line_dir, halfspace_normal, halfspace_dist)
+        allowed = intersect_ranges(allowed, this_allowed)
+    return allowed
+
+def find_maximum_ds_over_dt(s):
+    assert range_is_valid(get_allowable_d2s_over_dt2_range(s, 0))
+    a = 1
+    while range_is_valid(get_allowable_d2s_over_dt2_range(s, a)):
+        a *= 2
+    # a is invalid
+    # breakpoint is in (0, a]; binary search to find it
+    breakpoint_range = 0, a
+    for i in xrange(20):
+        if range_is_valid(get_allowable_d2s_over_dt2_range(s, (breakpoint_range[0] + breakpoint_range[1])/2)):
+            breakpoint_range = (breakpoint_range[0] + breakpoint_range[1])/2, breakpoint_range[1]
+        else:
+            breakpoint_range = breakpoint_range[0], (breakpoint_range[0] + breakpoint_range[1])/2
+    #print breakpoint_range
+    return breakpoint_range[0]
+
+def advance((s, ds_over_dt), d2s_over_dt2, ds):
+    # advance
+    if ds_over_dt**2 + 2 * ds * d2s_over_dt2 <= 0:
+        ds_over_dt = 0
+    else:
+        ds_over_dt = math.sqrt(ds_over_dt**2 + 2 * ds * d2s_over_dt2)
+    return s + ds, ds_over_dt
+
+def can_stop_from((s, ds_over_dt), ds):
+    while True:
+        if ds_over_dt == 0: return True
+        if s >= 1: return False
+        rng = get_allowable_d2s_over_dt2_range(s, ds_over_dt)
+        if not range_is_valid(rng): return False
+        s, ds_over_dt = advance((s, ds_over_dt), rng[0], ds)
+
+N = 1001
+
+px3 = []
+ds_over_dt = 0
+s = 0
+ds = 1/(N-1)
+res = []
+while True:
+    res.append((s, ds_over_dt))
+    print s, ds_over_dt,
+    if s != 0 and ds_over_dt == 0:
+        print
+        break
+    rng = get_allowable_d2s_over_dt2_range(s, ds_over_dt)
+    print can_stop_from(advance((s, ds_over_dt), rng[1], ds), ds)
+    if not range_is_valid(rng): break
+    if can_stop_from(advance((s, ds_over_dt), rng[1], ds), ds):
+        chosen = rng[1]
+    else:
+        assert can_stop_from(advance((s, ds_over_dt), rng[0], ds), ds)
+        chosen = rng[0]
+    px3.append((s+ds/2, chosen))
+    s, ds_over_dt = advance((s, ds_over_dt), chosen, ds)
+
+t = 0
+ts = [(0, t)]
+for (s1, ds_over_dt1), (s2, ds_over_dt2) in zip(res[:-1], res[1:]):
+    # circular integration..!
+    # find new_t such that a circle that goes (s1, t) and (s2, new_t)
+    # while respecting the slope conditions at each point.
+    # avoids divide by zero that arises from standard integration.
+    # consequences of inventing new math: unknown.
+    t = t + (s2 - s1) * (1 - ds_over_dt1*ds_over_dt2 + math.sqrt((1+ds_over_dt1**2)*(1+ds_over_dt2**2)))/(ds_over_dt1+ds_over_dt2)
+    ts.append((s2, t))
+
+res2 = []
+s = 0
+last_s = None
+for i in xrange(N):
+    if s > 1:
+        s2 = 1
+        assert i == N-1
+    else:
+        s2 = s
+    speval = bs.evaluate(s2)
+    assert ts[i][0] == s
+    t = ts[i][1]
+    assert res[i][0] == s
+    ds_over_dt = res[i][1]
+    if i == N-1:
+        d2s_over_dt2 = 0
+    else:
+        assert px3[i][0] == s+ds/2
+        d2s_over_dt2 = px3[i][1]
+    p = speval['p']
+    v = speval['v'] * ds_over_dt
+    a = speval['a'] * ds_over_dt**2 + speval['v'] * d2s_over_dt2
+    res2.append(dict(
+        s=s,
+        t=t,
+        ds_over_dt=ds_over_dt,
+        d2s_over_dt2=d2s_over_dt2,
+        p=p,
+        v=v,
+        a=a,
+        u=m*a - w(p, v),
+    ))
+    last_s = s
+    s += ds
+
 if 0:
+    pyplot.plot(*zip(*[(x['t'], x['u'][0]) for x in res2]))
+    pyplot.plot(*zip(*[(x['t'], x['u'][1]) for x in res2]))
+    pyplot.show()
+else:
+    spline_sampled_points = [bs.evaluate(x)['p'] for x in numpy.linspace(0, 1, 1000)]
     def animate(i):
         pyplot.cla()
-        pyplot.plot(*zip(*spline_view_points))
+        pyplot.gca().set_aspect('equal')
+        pyplot.plot(*zip(*spline_sampled_points))
+        pyplot.scatter(*zip(*spline_control_points))
         import time
-        t = time.time() % 10 / 10
-        res = bs.evaluate(t)
-        pyplot.scatter(*zip(*points))
-        pyplot.scatter(*zip(*[res['p']]) + [80])
-        pyplot.arrow(*(list(res['p']) + list(.1*res['v'])))
-        pyplot.arrow(*(list(res['p']) + list(.003*res['a'])))
+        t = time.time() % res2[-1]['t']
+        i = min(xrange(N), key=lambda i: abs(res2[i]['t'] - t))
+        inst = res2[i]
+        pyplot.scatter(*zip(*[inst['p']]) + [80])
+        #pyplot.arrow(*(list(inst['p']) + list(.1*inst['v'])))
+        pyplot.arrow(*(list(inst['p']) + list(.1*inst['u'])))
+        pyplot.plot(*zip(*[inst['p'] + .1*numpy.array(x) for x in [(-1, -1), (-1, 1), (1, 1), (1, -1), (-1, -1)]]))
 
     fig = pyplot.figure()
 
     ani = animation.FuncAnimation(fig, animate, interval=25)
 
     pyplot.show()
-
-else:
-    inf = 1e1000
-    
-    def w(p, v):
-        return -v
-    m = 1
-    
-    def line_halfspace_intersection(line_start, line_dir, halfspace_normal, halfspace_dist):
-        # halfspace_normal * allowed_point >= halfspace_dist
-        # line is defined by line_start + x * line_dir
-        # returns interval of x that is allowed
-        
-        # halfspace_normal . p >= halfspace_dist
-        # halfspace_normal . (line_start + x * line_dir) >= halfspace_dist
-        # halfspace_normal . line_start + x * halfspace_normal . line_dir >= halfspace_dist
-        # x * (halfspace_normal . line_dir) >= halfspace_dist - halfspace_normal . line_start
-        if halfspace_normal.dot(line_dir) > 0:
-            return ((halfspace_dist - halfspace_normal.dot(line_start)) / halfspace_normal.dot(line_dir), inf)
-        elif halfspace_normal.dot(line_dir) < 0:
-            return (-inf, (halfspace_dist - halfspace_normal.dot(line_start)) / halfspace_normal.dot(line_dir))
-        else: # line is parallel to halfspace boundary
-            if halfspace_normal.dot(line_start) >= halfspace_dist: # everything allowed
-                return (-inf, inf)
-            else: # nothing allowed
-                return (inf, -inf)
-    
-    def get_allowable_d2s_over_dt2_range(s, ds_over_dt):
-        res = bs.evaluate(s)
-        
-        # u = p + d2s/dt2 v
-        p = m * res['a'] * ds_over_dt**2 - w(res['p'], res['v'] * ds_over_dt)
-        v = m * res['v']
-        
-        #print p, v
-        
-        allowed = (-inf, inf)
-        for halfspace_normal, halfspace_dist in [
-            (numpy.array([1, 0]), -1),
-            (numpy.array([-1, 0]),-1),
-            (numpy.array([0, 1]), -1),
-            (numpy.array([0, -1]),-1),
-        ]:
-            this_allowed = line_halfspace_intersection(p, v, halfspace_normal, halfspace_dist)
-            #print this_allowed
-            allowed = max(allowed[0], this_allowed[0]), min(allowed[1], this_allowed[1])
-        return allowed
-    
-    range_is_valid = lambda (lo, hi): lo <= hi
-    
-    def find_maximum_ds_over_dt(s):
-        assert range_is_valid(get_allowable_d2s_over_dt2_range(s, 0))
-        a = 1
-        while range_is_valid(get_allowable_d2s_over_dt2_range(s, a)):
-            a *= 2
-        # a is invalid
-        # breakpoint is in (0, a]
-        breakpoint_range = 0, a
-        for i in xrange(20):
-            if range_is_valid(get_allowable_d2s_over_dt2_range(s, (breakpoint_range[0] + breakpoint_range[1])/2)):
-                breakpoint_range = (breakpoint_range[0] + breakpoint_range[1])/2, breakpoint_range[1]
-            else:
-                breakpoint_range = breakpoint_range[0], (breakpoint_range[0] + breakpoint_range[1])/2
-        #print breakpoint_range
-        return breakpoint_range[0]
-    
-    def advance((s, ds_over_dt), d2s_over_dt2, ds):
-        if ds_over_dt**2 + 2 * ds * d2s_over_dt2 <= 0:
-            ds_over_dt = 0
-        else:
-            ds_over_dt = math.sqrt(ds_over_dt**2 + 2 * ds * d2s_over_dt2)
-        return s + ds, ds_over_dt
-    
-    def can_stop_from((s, ds_over_dt), ds):
-        while True:
-            if ds_over_dt == 0: return True
-            if s >= 1: return False
-            rng = get_allowable_d2s_over_dt2_range(s, ds_over_dt)
-            if not range_is_valid(rng): return False
-            s, ds_over_dt = advance((s, ds_over_dt), rng[0], ds)
-    
-    N = 1001
-    
-    if 0:
-        px = [(x, numpy.linalg.norm(bs.evaluate(x)['v'])) for x in numpy.linspace(0, 1, N)]
-        pyplot.plot(*zip(*px))
-        
-        px = [(x, find_maximum_ds_over_dt(x)) for x in numpy.linspace(0, 1, N)]
-        pyplot.plot(*zip(*px))
-        
-        px = [(x, get_allowable_d2s_over_dt2_range(x, 0)[0]) for x in numpy.linspace(0, 1, N)]
-        pyplot.plot(*zip(*px))
-        px = [(x, get_allowable_d2s_over_dt2_range(x, 0)[1]) for x in numpy.linspace(0, 1, N)]
-        pyplot.plot(*zip(*px))
-        
-        px = [(x, get_allowable_d2s_over_dt2_range(x, find_maximum_ds_over_dt(x))[0]) for x in numpy.linspace(0, 1, N)]
-        pyplot.plot(*zip(*px))
-        px = [(x, get_allowable_d2s_over_dt2_range(x, find_maximum_ds_over_dt(x))[1]) for x in numpy.linspace(0, 1, N)]
-        pyplot.plot(*zip(*px))
-        
-        pyplot.show()
-    else:
-        px = []
-        px1 = []
-        px2 = []
-        px3 = []
-        ds_over_dt = 0
-        s = 0
-        ds = 1/(N-1)
-        res = []
-        while True:
-            px.append((s, ds_over_dt))
-            res.append((s, ds_over_dt))
-            print s, ds_over_dt,
-            if s != 0 and ds_over_dt == 0:
-                print
-                break
-            rng = get_allowable_d2s_over_dt2_range(s, ds_over_dt)
-            px1.append((s, rng[0]))
-            px2.append((s, rng[1]))
-            print can_stop_from(advance((s, ds_over_dt), rng[1], ds), ds)
-            if not range_is_valid(rng): break
-            if can_stop_from(advance((s, ds_over_dt), rng[1], ds), ds):
-                chosen = rng[1]
-            else:
-                assert can_stop_from(advance((s, ds_over_dt), rng[0], ds), ds)
-                chosen = rng[0]
-            px3.append((s+ds/2, chosen))
-            s, ds_over_dt = advance((s, ds_over_dt), chosen, ds)
-        
-        t = 0
-        ts = [(0, t)]
-        for (s1, ds_over_dt1), (s2, ds_over_dt2) in zip(res[:-1], res[1:]):
-            # circular integration..!
-            # find new_t such that a circle that goes (s1, t) and (s2, new_t)
-            # while respecting the slope conditions at each point.
-            # avoids divide by zero that arises from standard integration.
-            # consequences of inventing new math: unknown.
-            t = t + (s2 - s1) * (1 - ds_over_dt1*ds_over_dt2 + math.sqrt((1+ds_over_dt1**2)*(1+ds_over_dt2**2)))/(ds_over_dt1+ds_over_dt2)
-            ts.append((s2, t))
-        
-        if 0:
-            pyplot.plot(*zip(*ts))
-            
-            pyplot.plot(*zip(*px))
-            #pyplot.plot(*zip(*px1))
-            #pyplot.plot(*zip(*px2))
-            #pyplot.plot(*zip(*px3))
-            px = [(x, find_maximum_ds_over_dt(x)) for x in numpy.linspace(0, 1, N)]
-            pyplot.plot(*zip(*px))
-            pyplot.show()
-        else:
-            res2 = []
-            s = 0
-            last_s = None
-            for i in xrange(N):
-                if s > 1:
-                    s2 = 1
-                    assert i == N-1
-                else:
-                    s2 = s
-                speval = bs.evaluate(s2)
-                assert ts[i][0] == s
-                t = ts[i][1]
-                assert res[i][0] == s
-                ds_over_dt = res[i][1]
-                if i == 0:
-                    left_d2s_over_dt2 = 0
-                    assert px3[i][0] == s+ds/2
-                    right_d2s_over_dt2 = px3[i][1]
-                elif i == N-1:
-                    assert px3[i-1][0] == last_s+ds/2
-                    left_d2s_over_dt2 = px3[i-1][1]
-                    right_d2s_over_dt2 = 0
-                else:
-                    assert px3[i-1][0] == last_s+ds/2
-                    left_d2s_over_dt2 = px3[i-1][1]
-                    assert px3[i][0] == s+ds/2
-                    right_d2s_over_dt2 = px3[i][1]
-                d2s_over_dt2 = (left_d2s_over_dt2 + right_d2s_over_dt2)/2
-                d2s_over_dt2 = right_d2s_over_dt2
-                p = speval['p']
-                v = speval['v'] * ds_over_dt
-                a = speval['a'] * ds_over_dt**2 + speval['v'] * d2s_over_dt2
-                res2.append(dict(
-                    s=s,
-                    t=t,
-                    ds_over_dt=ds_over_dt,
-                    d2s_over_dt2=d2s_over_dt2,
-                    p=p,
-                    v=v,
-                    a=a,
-                    u=m*a - w(p, v),
-                ))
-                last_s = s
-                s += ds
-            
-            if 0:
-                pyplot.plot(*zip(*[(x['t'], x['u'][0]) for x in res2]))
-                pyplot.plot(*zip(*[(x['t'], x['u'][1]) for x in res2]))
-                pyplot.show()
-            else:
-                def animate(i):
-                    pyplot.cla()
-                    pyplot.gca().set_aspect('equal')
-                    pyplot.plot(*zip(*spline_view_points))
-                    pyplot.scatter(*zip(*points))
-                    import time
-                    t = time.time() % res2[-1]['t']
-                    i = min(xrange(N), key=lambda i: abs(res2[i]['t'] - t))
-                    inst = res2[i]
-                    pyplot.scatter(*zip(*[inst['p']]) + [80])
-                    #pyplot.arrow(*(list(inst['p']) + list(.1*inst['v'])))
-                    pyplot.arrow(*(list(inst['p']) + list(.1*inst['u'])))
-                    pyplot.plot(*zip(*[inst['p'] + .1*numpy.array(x) for x in [(-1, -1), (-1, 1), (1, 1), (1, -1), (-1, -1)]]))
-
-                fig = pyplot.figure()
-
-                ani = animation.FuncAnimation(fig, animate, interval=25)
-
-                pyplot.show()
